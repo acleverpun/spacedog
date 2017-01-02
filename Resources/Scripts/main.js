09dc40b55fc446794c375347c11f7ff3754e0d58
Ross Hadden <rosshadden@gmail.com>
2017-01-01 22:37:25 -0500

    Removed generated files from repo, and added package.json.

--- Resources/Scripts/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var scene = Atomic.player.loadScene('Scenes/Scene.scene');
-// called per frame, optional
-function update(timeStep) {
-    // Handle update
-}
-exports.update = update;
