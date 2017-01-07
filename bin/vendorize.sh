9a97bb9affc53eac8c4a0f72d4a4e2a2f60aedf3
Ross Hadden <rosshadden@gmail.com>
2017-01-07 12:04:59 -0500

    Even better-looking vendorize.sh.

--- bin/vendorize.sh
+++ bin/vendorize.sh
@@ -4,13 +4,14 @@ file="./vendors.js"
 
 # TODO: handle flipped `dependencies` and `devDependencies`
 packages=$(
-	# sed -r -n -e '/devDependencies/,/dependencies/ p' package.json \
 	sed -r -n -e '/dependencies/,/devDependencies/ p' package.json \
 		| tail -n +2 \
 		| head -n -2 \
 		| sed -r 's;\s*"(.*)": "(.*)";\t"\1": require("\1");'
 )
 
-echo "module.exports = {" > $file
-echo "$packages" >> $file
-echo "};" >> $file
+cat > $file << EOL
+module.exports = {
+$packages
+};
+EOL
