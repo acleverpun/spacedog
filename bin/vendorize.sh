71d0ab724ff15c08b20cd48b71dfe637b6d52916
Ross Hadden <rosshadden@gmail.com>
2017-01-07 11:46:36 -0500

    Improved vendorize code.

--- bin/vendorize.sh
+++ bin/vendorize.sh
@@ -2,12 +2,14 @@
 
 file="./vendors.js"
 
+# TODO: handle flipped `dependencies` and `devDependencies`
+packages=$(
+	sed -r -n -e '/dependencies/,/devDependencies/ p' package.json \
+		| head -2 \
+		| tail -1 \
+		| sed -r 's;\s*"(.*)": "(.*)",?;\t"\1": require("\1"),;'
+)
+
 echo "module.exports = {" > $file
-
-sed -r -n -e '/dependencies/,/devDependencies/ p' package.json \
-	| head -2 \
-	| tail -1 \
-	| sed -r 's;\s*"(.*)": "(.*)",?;\t"\1": require("\1"),;' \
-	>> $file
-
+echo "$packages" >> $file
 echo "};" >> $file
--- vendorize.js
+++ vendorize.js
@@ -1,18 +1,7 @@
 var packages = require('./vendors');
 
-var debug = false;
 var origModSearch = Duktape.modSearch;
-
 Duktape.modSearch = function(id, require, exports, module) {
-	if (packages[id]) {
-		var result = packages[id];
-		if (debug) console.log('Loading vendor module: ' + id);
-		for (var mod in result) {
-			if (debug) console.log('Mapping export: ' + mod);
-			exports[mod] = result[mod];
-		}
-	} else {
-		if (debug) console.log('Loading other module: ' + id);
-		return origModSearch(id, require, exports, module);
-	}
+	if (!packages[id]) return origModSearch(id, require, exports, module);
+	module.exports = packages[id];
 };
