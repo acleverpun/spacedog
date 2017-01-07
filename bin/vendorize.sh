2d3c4a51c8eca6fd58126a8c6da45099c3fb201f
Ross Hadden <rosshadden@gmail.com>
2017-01-07 11:56:55 -0500

    Properly handle any number of packages.

--- bin/vendorize.sh
+++ bin/vendorize.sh
@@ -4,10 +4,11 @@ file="./vendors.js"
 
 # TODO: handle flipped `dependencies` and `devDependencies`
 packages=$(
+	# sed -r -n -e '/devDependencies/,/dependencies/ p' package.json \
 	sed -r -n -e '/dependencies/,/devDependencies/ p' package.json \
-		| head -2 \
-		| tail -1 \
-		| sed -r 's;\s*"(.*)": "(.*)",?;\t"\1": require("\1"),;'
+		| tail -n +2 \
+		| head -n -2 \
+		| sed -r 's;\s*"(.*)": "(.*)";\t"\1": require("\1");'
 )
 
 echo "module.exports = {" > $file
